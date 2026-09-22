f90a2cfae913770f9fc910a5e391950be2051052
wilco73 <wilco73@hotmail.fr>
2026-09-22 04:47:19 +0200

    Update MainLayout.jsx

--- client/src/components/MainLayout.jsx
+++ client/src/components/MainLayout.jsx
@@ -3,7 +3,7 @@ import {
   Menu, X, Home, Trophy, User, History, Settings, LogOut, 
   Users, Star, Crown, ChevronDown, ChevronRight,
   FileQuestion, Palette, Trash, Monitor, Check, Eye, EyeOff, Grid, Image,
-  Gamepad2, Smile
+  Gamepad2, Smile, Gem
 } from 'lucide-react';
 import DarkModeToggle from './DarkModeToggle';
 import Avatar from './Avatar';
